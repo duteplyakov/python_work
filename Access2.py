f5ce3d42319b3233a6cb6ad1f7499e7a235f33c5
duteplyakov <sotnik2001@yandex.ru>
2022-12-16 13:58:44 +0300

    Add file

--- Access2.py
+++ Access2.py
@@ -22,7 +22,7 @@ f = open("name.txt", "r") #подгружаем файл с именами
 
 with open("name.txt", "rt") as file: #проверка сперва по фамилии , потом по возрасту
     print(f"Отлично, вы: {full_name}, доступ разрешен!" if f"{last_name}" in file.read() and f"{now1}" >= "30"
-          else "В доступе отказано!")
+          else "В доступе отказано!!!")
 
 
 
