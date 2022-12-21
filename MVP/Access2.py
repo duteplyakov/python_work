5cca5b1bb36b97cc1f0a656740f3ff825cd1c03f
duteplyakov <sotnik2001@yandex.ru>
2022-12-21 17:32:08 +0300

    Merge remote-tracking branch 'origin/master'

--- MVP/Access2.py
+++ MVP/Access2.py
@@ -18,9 +18,9 @@ full_name = f"{first_name} {last_name}, твой возраст {now1}"
 #итог
 
 message = f"Привет, {full_name}!"
-f = open("../name.txt", "r") #подгружаем файл с именами
+f = open("name.txt", "r") #подгружаем файл с именами
 
-with open("../name.txt", "rt") as file: #проверка сперва по фамилии , потом по возрасту
+with open("name.txt", "rt") as file: #проверка сперва по фамилии , потом по возрасту
     print(f"Отлично, вы: {full_name}, доступ разрешен!" if f"{last_name}" in file.read() and f"{now1}" >= "30"
           else "В доступе отказано!!!")
 
--- MVP/card4.py
+++ MVP/card4.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 
 # загрузка файла
-excel_data = pd.read_excel('cardsell.xlsx')
+excel_data = pd.read_excel('MVP/cardsell.xlsx')
 # Чтение файла в фрейме
 data = pd.DataFrame(excel_data, columns=['Тариф', 'Выдача', 'Активация', 'ФИО'])
 # Вывод результата
 print (data.groupby(['ФИО', 'Тариф']).count())
 
 #печатаю вывод в файл
-with open('../outcard.txt', 'w') as f:
+with open('MVP/outcard.txt', 'w') as f:
     dfAsString = data.to_string(header=True, index=False)
     f.write(dfAsString)
 
